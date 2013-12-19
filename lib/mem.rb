8f4b898c151566b0c3a754878ddedadd42881f45
Ryo Nakamura <r7kamura@gmail.com>
2013-12-19 23:52:30 +0900

    Implement Mem module

--- lib/mem.rb
+++ lib/mem.rb
@@ -1,5 +1,16 @@
 require "mem/version"
+require "active_support/core_ext/module/aliasing"
 
 module Mem
-  # Your code goes here...
+  def memoize(method_name)
+    define_method("#{method_name}_with_memoize") do |*args, &block|
+      if instance_variable_defined?("@#{method_name}")
+        instance_variable_get("@#{method_name}")
+      else
+        instance_variable_set("@#{method_name}", send("#{method_name}_without_memoize", *args, &block))
+      end
+    end
+    alias_method "#{method_name}_without_memoize", method_name
+    alias_method method_name, "#{method_name}_with_memoize"
+  end
 end
---
+++ spec/mem_spec.rb
@@ -0,0 +1,29 @@
+require "spec_helper"
+
+describe Mem do
+  describe "#memoize" do
+    let(:klass) do
+      Class.new do
+        extend Mem
+
+        def foo(&block)
+          bar(&block)
+        end
+        memoize :foo
+
+        def bar
+          yield
+        end
+      end
+    end
+
+    let(:object) do
+      klass.new
+    end
+
+    it "memoizes the result of specified method call" do
+      expect(object).to receive(:bar).once.and_call_original
+      expect(object.foo { "foo" }).to eq "foo"
+    end
+  end
+end
